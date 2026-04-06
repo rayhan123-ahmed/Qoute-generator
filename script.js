d084cc157e6a14977c9176db6821af8114e289e3
Rayhan Ahmed <rmd09181@gmail.com>
2026-04-06 17:31:15 +0600

    fix: correct quote formatting and improve function comments

--- script.js
+++ script.js
@@ -3,36 +3,61 @@ const qouteText = document.querySelector(".quote-text");
 const qouteAuthor = document.querySelector(".quote-author");
 const generateBtn = document.querySelector(".generate-btn");
 
-// creat array to store the qoutes
+
 const quotes = [
   {
     quote: "The best way to get started is to quit talking and begin doing.",
-    author: "Walt Disney"
+    author: "Walt Disney",
   },
   {
     quote: "Don't let yesterday take up too much of today.",
-    author: "Will Rogers"
+    author: "Will Rogers",
   },
   {
     quote: "It's not whether you get knocked down, it's whether you get up.",
-    author: "Vince Lombardi"
+    author: "Vince Lombardi",
   },
   {
-    quote: "If you are working on something exciting, it will keep you motivated.",
-    author: "Steve Jobs"
+    quote:
+      "If you are working on something exciting, it will keep you motivated.",
+    author: "Steve Jobs",
   },
   {
     quote: "Success is not in what you have, but who you are.",
-    author: "Bo Bennett"
-  }
+    author: "Bo Bennett",
+  },
+  {
+    quote: "Dream big and dare to fail.",
+    author: "Norman Vaughan",
+  },
+  {
+    quote: "Do something today that your future self will thank you for.",
+    author: "Sean Patrick Flanery",
+  },
+  {
+    quote: "Hard work beats talent when talent doesn't work hard.",
+    author: "Tim Notke",
+  },
+  {
+    quote: "Stay hungry, stay foolish.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "Discipline is choosing between what you want now and what you want most.",
+    author: "Abraham Lincoln",
+  },
 ];
 
-//  qoute generate funtion
-function generteQoute() {
-    let randomQoute = Math.floor(Math.random()*quotes.length)
 
-    qouteText.textContent = quotes[randomQoute].quote;
-    qouteAuthor.textContent = '- '+quotes[randomQoute].author
+// quote generate function
+
+ function generteQoute() {
+   let randomQoute = Math.floor(Math.random()*quotes.length)
+    
+   qouteText.textContent = quotes[randomQoute].quote;
+   qouteAuthor.textContent = '- ' +quotes[randomQoute].author
+  
 }
 
 // add eventListner to button
